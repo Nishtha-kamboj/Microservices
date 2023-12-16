5fdb9d84fd4cdb3a3fb2a1b90028b21d0fb41a7b
Nishtha-kamboj <nishthakamboj0519@gmail.com>
2023-12-16 10:21:20 -0500

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Hello World!!! This is Nishitha and it's my frontend service!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
